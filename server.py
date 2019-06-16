05d7165f619298eccd44672d548e195508d447ab
August Ye <augustye@gmail.com>
2019-06-16 16:43:44 +0800

    init

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def display_init():
 
 def display(x, y, text):
   screen = pygame.display.set_mode((480,320))
-  font = pygame.font.SysFont("comicsansms", 256)
+  font = pygame.font.SysFont("comicsansms", 128)
   text = font.render(str(text), True, (0, 128, 0))
   screen.fill((255, 255, 255))
   screen.blit(text,(x, y))
