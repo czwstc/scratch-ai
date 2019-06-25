b5b21bb0e2091154d1fb03af771e9a810ca358f8
ZweiChen <zweichan@outlook.com>
2019-06-25 22:50:13 +0800

    Change the way of installing grpcio, opencv
    delete python3-grpcio  python3-opencv 
    and add pip3 install grpcio opencv

--- install/raspberry_pi.sh
+++ install/raspberry_pi.sh
@@ -2,7 +2,7 @@
 # Based on: 2019-06-20-raspbian-buster-full
 
 #apt packages
-sudo apt install python3-pip python3-h5py python3-scipy python3-grpcio python3-opencv\
+sudo apt install python3-pip python3-h5py python3-scipy \
     libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libportmidi-dev \
     libatlas-base-dev libjasper1 libilmbase-dev libopenexr-dev libgstreamer1.0-dev libswscale-dev \
     libavformat-dev libavcodec-dev libpng-dev libjpeg-dev libhdf5-dev gfortran 
@@ -17,5 +17,5 @@ EOF
 exit
 
 #pip packages
-sudo pip3 install pygame flask Pillow picamera tensorflow
+sudo pip3 install pygame flask Pillow picamera tensorflow opencv grpcio
 sudo python3 -c "from tensorflow.python.client import device_lib; print(device_lib.list_local_devices())"
